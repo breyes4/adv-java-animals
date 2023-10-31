32d19ff99b348e3683d6c0865f3c36669119b1ee
breyes4 <71798440+breyes4@users.noreply.github.com>
2023-10-30 21:29:22 -0500

    Main.java

--- src/us/mattgreen/Main.java
+++ src/us/mattgreen/Main.java
@@ -8,26 +8,31 @@ public class Main {
     private final static FileInput inFile = new FileInput("animals.txt");
 
     public static void main(String[] args) {
-        ArrayList<Talkable> zoo = new ArrayList<>();
 
-        // Lines to Replace Begin Here
-        zoo.add(new Dog(true, "Bean"));
-        zoo.add(new Cat(9, "Charlie"));
-        zoo.add(new Teacher(44, "Stacy Read"));
-        // End Lines to Replace
+        try {
+            ArrayList<Talkable> zoo = new ArrayList<>();
 
-        for (Talkable thing : zoo) {
-            printOut(thing);
+            CreateAnimalCollection animalCollection = new CreateAnimalCollection(zoo);
+            animalCollection.askForAnimalType();
+            animalCollection.askForAnimalType();
+            animalCollection.askForAnimalType();
+
+            for (Talkable thing : zoo) {
+                printOut(thing);
+            }
+            outFile.fileClose();
+            inFile.fileRead();
+            inFile.fileClose();
+
+            FileInput indata = new FileInput("animals.txt");
+            String line;
+            while ((line = indata.fileReadLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        outFile.fileClose();
-        inFile.fileRead();
-        inFile.fileClose();
 
-        FileInput indata = new FileInput("animals.txt");
-        String line;
-        while ((line = indata.fileReadLine()) != null) {
-            System.out.println(line);
-        }
     }
 
     public static void printOut(Talkable p) {
